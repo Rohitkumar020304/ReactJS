111e37432d55ae5355b8a43b9e1d22a7a99b9320
Rohit <kumarrohit020304@gmail.com>
2025-05-12 01:43:34 +0530

    Added basic and vite React setups

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1> chai or react </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>chai or react with vite | Rohit</h1>
+  )
+}
+
+export default App
