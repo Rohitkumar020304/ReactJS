7ac05ace7381ed7cee88b05de2df562669a42145
Rohit <kumarrohit020304@gmail.com>
2025-06-28 00:57:29 +0530

    basic counter + - with hook

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  let [counter, setCounter] = useState(5);
+  function addValue() {
+    if(counter < 20)
+    counter++;
+    setCounter(counter)
+    console.log("Clicked add", counter);
+  }
+  const removeValue = () => {
+    if(counter > 0 ){
+      counter--;
+    }
+    setCounter(counter)
+    console.log("Clicked remove", counter);
+  }
+  return (
+    <>
+      <h1>kya dekhne aaye ho?</h1>
+      <h2>Counter value: {counter} </h2>
+
+      <button
+      onClick = {addValue}
+        > Add value {counter}</button>
+        
+      <br />
+      <button
+      onClick = {removeValue}
+      >remove value {counter}</button>
+      <footer>this is footer {counter}</footer>
+    </>
+  )
+}
+
+export default App
